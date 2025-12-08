7082b03538c69309c54973a93669a9b76d44f203
김창수 <chang9811@interplug.co.kr>
2025-12-08 10:04:10 +0900

    style: side nav ui 추가

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
   return (
     <header>
       <div className="header-inner">
@@ -9,7 +13,10 @@ export default function Header() {
             힘이 나는 커피생활 을지로3가점
           </div>
           <div className="header-menu-btn">
-            <button className="menu-btn"></button>
+            <button
+              className="menu-btn"
+              onClick={() => setIsSideNavOpen(true)}
+            ></button>
           </div>
         </div>
         <div className="header-body">
@@ -35,8 +42,55 @@ export default function Header() {
           </div>
         </div>
       </div>
-      <div className="side-nav-wrap">
-        <div className="side-nav-inner">sss</div>
+      <div className={`side-nav-wrap ${isSideNavOpen ? "act" : ""}`}>
+        <div className="side-nav-inner">
+          <div className="side-nav-header">
+            <div className="side-nav-header-inner">
+              <div className="side-nav-header-icon">
+                <Image
+                  src="/assets/images/common/avatar_icon.svg"
+                  alt="side-nav-header-icon"
+                  fill
+                />
+              </div>
+              <div className="side-nav-header-info">
+                <div className="side-nav-header-name">
+                  <span>홍길동</span>님 환영 합니다.
+                </div>
+                <div className="side-nav-header-company">Interplug corp.</div>
+              </div>
+            </div>
+            <button
+              className="side-close-btn"
+              onClick={() => setIsSideNavOpen(false)}
+            ></button>
+          </div>
+          <div className="side-nav-logout-wrap">
+            <button className="btn-form black block">로그아웃</button>
+          </div>
+          <div className="side-nav-body">
+            <ul className="side-nav-body-list">
+              <li className="side-nav-body-item">
+                <Link href="/">회원 정보 수정</Link>
+              </li>
+              <li className="side-nav-body-item">
+                <Link href="/">비밀번호 변경</Link>
+              </li>
+              <li className="side-nav-body-item">
+                <Link href="/">근무 시간 조회 및 출퇴근 체크</Link>
+              </li>
+              <li className="side-nav-body-item">
+                <Link href="/">근로계약서</Link>
+              </li>
+              <li className="side-nav-body-item">
+                <Link href="/">급여명세서</Link>
+              </li>
+              <li className="side-nav-body-item">
+                <Link href="/">TO-DO LIST</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </header>
   );
