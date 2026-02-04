3368308cf2034a93f2d567cf8a49d3ca41de1096
김창수 <chang9811@interplug.co.kr>
2026-02-04 16:18:35 +0900

    style: footer 수정

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,8 +3,9 @@ import { usePathname } from 'next/navigation'
 
 export default function Footer() {
   const pathname = usePathname()
+  const pathnameArray = ['/', '/request', '/signup']
   return (
-    <footer className={pathname === '/' ? 'main' : ''}>
+    <footer className={pathnameArray.includes(pathname) ? 'main' : ''}>
       <div className="footer-container">Copyrights© 2025 따름인 All Rights Reserved.</div>
     </footer>
   )
